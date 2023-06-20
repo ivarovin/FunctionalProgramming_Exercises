81782d8763a0a7b2da9b7f84c65e117e9ebdb9e6
Iván Moreno <imoreno.owo@gmail.com>
2023-06-20 19:48:36 +0200

    feat: add precondition to pass null value to Some

--- FunctionalExercises/Optional/Some.cs
+++ FunctionalExercises/Optional/Some.cs
@@ -4,7 +4,13 @@
     {
         readonly T value;
 
-        public Some(T value) => this.value = value;
+        public Some(T value)
+        {
+            if (value is null)
+                throw new ArgumentNullException(nameof(value), "It is necessary to pass some value");
+
+            this.value = value;
+        }
 
         public void Deconstruct(out T value) => value = this.value;
     }

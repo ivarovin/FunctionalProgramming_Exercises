3fe445cfacedbf4e7646e5a763832a46f11a9298
Iván Moreno <imoreno.owo@gmail.com>
2023-06-20 19:39:32 +0200

    feat: deconstruct Some returns assigned value if not null

---
+++ Tests/Optional/SomeTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+
+namespace IMoreno.FunctionalExercises.Optional.Tests
+{
+    public class SomeTests
+    {
+        [Fact]
+        public void Deconstruct_returns_assigned_value_if_not_null()
+        {
+            new Some<string>("Test").Deconstruct(out var result);
+
+            result.Should().Be("Test");
+        }
+    }
+
+    public record Some<T>
+    {
+        readonly T value;
+
+        public Some(T value) => this.value = value;
+
+        public void Deconstruct(out T value) => value = this.value;
+    }
+}

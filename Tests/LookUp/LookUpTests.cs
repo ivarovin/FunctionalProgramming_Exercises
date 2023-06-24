90f35b4fe511fa38de9cc0165666fb40ac98fecc
Iván Moreno <imoreno.owo@gmail.com>
2023-06-24 19:24:28 +0200

    test: LookUp: return first value that matches predicate

--- Tests/LookUp/LookUpTests.cs
+++ Tests/LookUp/LookUpTests.cs
@@ -6,9 +6,17 @@ namespace IMoreno.FunctionalExercises.LookUp.Tests
     public class LookUpTests
     {
         [Fact]
-        public void Return_none_if_no_value_matches_predicate()
+        public void Return_none_if_collection_is_empty()
         {
             new List<int>().LookUp(i => i == 0)
+                .Should()
+                .BeOfType<None<int>>();
+        }
+
+        [Fact]
+        public void Return_none_if_no_value_matches_predicate()
+        {
+            new List<int>() { 1 }.LookUp(i => i == 0)
                 .Should()
                 .BeOfType<None<int>>();
         }
@@ -19,6 +27,16 @@ namespace IMoreno.FunctionalExercises.LookUp.Tests
             new List<int>() { 0 }.LookUp(i => i == 0)
                 .Should()
                 .BeOfType<Some<int>>();
+        }
+
+        [Fact]
+        public void Return_first_value_that_matches_predicate()
+        {
+            bool IsOdd(int i) => i % 2 == 0;
+
+            new List<int>() { 2, 4, 6 }.LookUp(IsOdd)
+                .Should()
+                .Be(new Some<int>(2));
         }
     }
 }

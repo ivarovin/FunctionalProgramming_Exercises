8660a70b80a6e3ded33584aa36134a0198826a69
Iván Moreno <imoreno.owo@gmail.com>
2023-06-24 18:49:49 +0200

    feat: parse string to enum value

---
+++ FunctionalExercises/ParseDayOfWeek/Enum.cs
@@ -0,0 +1,10 @@
+﻿namespace IMoreno.FunctionalExercises.ParseDayOfWeek
+{
+    public static class Enum
+    {
+        public static T Parse<T>(string value) where T : struct
+        {
+            return System.Enum.Parse<T>(value);
+        }
+    }
+}
---
+++ Tests/ParseDayOfWeek/ParseDayOfWeekTests.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+
+namespace IMoreno.FunctionalExercises.ParseDayOfWeek.Tests
+{
+    public class ParseDayOfWeekTests
+    {
+        [Fact]
+        public void Parse_valid_DayOfWeek()
+        {
+            Enum.Parse<DayOfWeek>("Friday").Should().Be(DayOfWeek.Friday);
+        }
+    }
+}

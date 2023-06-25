78121e4852f59a5c177708ba09e0bd20688aa861
Iván Moreno <imoreno.owo@gmail.com>
2023-06-25 20:17:02 +0200

    feat: 6.2-BindAndReturn: bind options

---
+++ FunctionalExercises/6.2-BindAndReturn/MonadOperation.cs
@@ -0,0 +1,17 @@
+﻿using static IMoreno.FunctionalExercises.Optional.OptionType;
+using IMoreno.FunctionalExercises.Optional;
+
+namespace IMoreno.FunctionalExercises.BindAndReturn
+{
+    public static class MonadOperation
+    {
+        public static Option<R> Bind<T, R>(this Option<T> option, Func<T, Option<R>> op)
+        {
+            return option.Match
+                (
+                    none: () => None,
+                    some: value => op(value)
+                );
+        }
+    }
+}
---
+++ Tests/6.2-BindAndReturn/BindAndReturn.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using IMoreno.FunctionalExercises.Optional;
+
+namespace IMoreno.FunctionalExercises.BindAndReturn.Tests
+{
+    public class BindAndReturnTests
+    {
+        [Fact]
+        public void Bind_options()
+        {
+            Option<int> Duplicate(int value) => new Some<int>(value * 2);
+
+            new Some<int>(1)
+                .Bind(Duplicate)
+                .Bind(Duplicate)
+                .Should()
+                .Be(new Some<int>(4));
+        }
+    }
+}

8dbfa7e1897a94f751038f21ad145bcad4470cd4
Iván Moreno <imoreno.owo@gmail.com>
2023-06-25 13:15:37 +0200

    feat!: 6.1-Map: map returns collection with applied function

---
+++ FunctionalExercises/6.1-Map/CollectionOperation.cs
@@ -0,0 +1,12 @@
+﻿using IMoreno.FunctionalExercises.Optional;
+
+namespace IMoreno.FunctionalExercises.Map
+{
+    public static class CollectionOperation
+    {
+        public static ISet<R> Map<T, R>(this ISet<T> s, Func<T, R> opt)
+        {
+            return null;
+        }
+    }
+}
---
+++ Tests/6.1-Map/MapTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+
+namespace IMoreno.FunctionalExercises.Map.Tests
+{
+    public class MapTests
+    {
+        [Fact]
+        public void Map_returns_collection_with_applied_function()
+        {
+            int Duplicate(int x) => x * 2;
+
+            new HashSet<int>() { 1 }.Map(Duplicate)
+                .Should()
+                .HaveCount(1).And
+                .Contain(2);
+        }
+    }
+}

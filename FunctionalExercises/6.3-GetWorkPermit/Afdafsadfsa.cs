ab78279060d6d3d515b707a9f613c47bcaffb7b7
Iván Moreno <imoreno.owo@gmail.com>
2023-06-26 17:09:29 +0200

    chore: 6.3-GetWorkPermit: add exercise code snippet

---
+++ FunctionalExercises/6.3-GetWorkPermit/Afdafsadfsa.cs
@@ -0,0 +1,23 @@
+﻿using IMoreno.FunctionalExercises.Optional;
+
+namespace IMoreno.FunctionalExercises.GetWorkPermit
+{
+    public static class Afdafsadfsa
+    {
+        Option<WorkPermit> GetWorkPermit(Dictionary<string, Employee> employees, string employeeId) { }// your implementation here...
+        double AverageYearsWorkedAtTheCompany(List<Employee> employees) { } // your implementation here...
+
+        public record Employee
+        (
+            string Id,
+            Option<WorkPermit> WorkPermit, DateTime JoinedOn,
+            Option<DateTime> LeftOn
+        );
+
+        public record WorkPermit
+        (
+            string Number,
+            DateTime Expiry
+        );
+    }
+}

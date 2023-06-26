53248280905860f1c9f38425ada9c080a8f4536f
Iván Moreno <imoreno.owo@gmail.com>
2023-06-26 19:25:39 +0200

    test: 6.3-GetWorkPermit: extract duplicated code to properties

--- Tests/6.3-GetWorkPermit/GetWorkPermitTests.cs
+++ Tests/6.3-GetWorkPermit/GetWorkPermitTests.cs
@@ -1,37 +1,34 @@
-﻿using static IMoreno.FunctionalExercises.Optional.OptionType;
-using IMoreno.FunctionalExercises.Optional;
+﻿using IMoreno.FunctionalExercises.Optional;
 using FluentAssertions;
 
 namespace IMoreno.FunctionalExercises.GetWorkPermit.Tests
 {
     public class GetWorkPermitTests
     {
+        WorkPermit WorkPermit => new WorkPermit("1a", DateTime.Today);
+        Dictionary<string, Employee> Employees => new()
+        {
+            { "MyId", new Employee("1", WorkPermit, DateTime.Today, DateTime.Today) }
+        };
+
         [Fact]
         public void Get_work_permit_of_employee()
         {
-            var sut = new Afdafsadfsa();
-            var employees = new Dictionary<string, Employee>();
-            var workPermit = new WorkPermit("1a", DateTime.Now);
-            employees.Add("1", new Employee("1", workPermit, DateTime.Now, DateTime.Now));
-
-            sut.GetWorkPermit(employees, "1")
+            new Afdafsadfsa()
+                .GetWorkPermit(Employees, "MyId")
                 .Match(() => default, result => result)
                 .Should()
-                .Be(workPermit);
+                .Be(WorkPermit);
         }
 
         [Fact]
         public void Work_permit_is_none_when_expired()
         {
-            var sut = Afdafsadfsa.ForDay(DateTime.Today);
-            var employees = new Dictionary<string, Employee>();
-            var workPermit = new WorkPermit("1a", DateTime.Today.AddDays(-1));
-            employees.Add("1", new Employee("1", workPermit, DateTime.Now, DateTime.Now));
-
-            sut.GetWorkPermit(employees, "1")
-                .Match(() => false, result => true)
-                .Should()
-                .BeFalse();
+            Afdafsadfsa.ForDay(DateTime.Today)
+                       .GetWorkPermit(Employees, "MyId")
+                       .Match(() => false, result => true)
+                       .Should()
+                       .BeFalse();
         }
     }
 }

5a2aea2cd74de666a491a64ea91eece7a7df30c2
Iván Moreno <imoreno.owo@gmail.com>
2023-06-26 17:20:45 +0200

    test!: 6.3-GetWorkPermit: get work permit of employee

--- FunctionalExercises/6.3-GetWorkPermit/Afdafsadfsa.cs
+++ FunctionalExercises/6.3-GetWorkPermit/Afdafsadfsa.cs
@@ -2,22 +2,23 @@
 
 namespace IMoreno.FunctionalExercises.GetWorkPermit
 {
-    public static class Afdafsadfsa
+    public class Afdafsadfsa
     {
-        Option<WorkPermit> GetWorkPermit(Dictionary<string, Employee> employees, string employeeId) { }// your implementation here...
-        double AverageYearsWorkedAtTheCompany(List<Employee> employees) { } // your implementation here...
-
-        public record Employee
-        (
-            string Id,
-            Option<WorkPermit> WorkPermit, DateTime JoinedOn,
-            Option<DateTime> LeftOn
-        );
-
-        public record WorkPermit
-        (
-            string Number,
-            DateTime Expiry
-        );
+        public Option<WorkPermit> GetWorkPermit(Dictionary<string, Employee> employees, string employeeId) { return null; }// your implementation here...
+        public double AverageYearsWorkedAtTheCompany(List<Employee> employees) { return 0; } // your implementation here...
     }
+
+    public record Employee
+    (
+        string Id,
+        Option<WorkPermit> WorkPermit,
+        DateTime JoinedOn,
+        Option<DateTime> LeftOn
+    );
+
+    public record WorkPermit
+    (
+        string Number,
+        DateTime Expiry
+    );
 }
---
+++ Tests/6.3-GetWorkPermit/GetWorkPermitTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using IMoreno.FunctionalExercises.Optional;
+
+namespace IMoreno.FunctionalExercises.GetWorkPermit.Tests
+{
+    public class GetWorkPermitTests
+    {
+        [Fact]
+        public void Get_work_permit_of_employee()
+        {
+            var sut = new Afdafsadfsa();
+            var employees = new Dictionary<string, Employee>();
+            employees.Add("1", new Employee("1", new WorkPermit("1a", DateTime.Now), DateTime.Now, DateTime.Now));
+
+            sut.GetWorkPermit(employees, "1")
+                .Match(() => default, result => result)
+                .Should()
+                .Be(new WorkPermit("1a", DateTime.Now));
+        }
+    }
+}

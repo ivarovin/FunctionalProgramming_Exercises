4dadfbd4c7fe8b86e316164184d09089cce26251
Iván Moreno <imoreno.owo@gmail.com>
2023-06-29 18:02:15 +0200

    feat: 7.3: compose two unary functions into one

---
+++ FunctionalExercises/7.3-Compose/Composition.cs
@@ -0,0 +1,10 @@
+﻿namespace IMoreno.FunctionalExercises.Compose
+{
+    public static class Composition
+    {
+        public static Func<T, T> Of<T>(Func<T, T> f, Func<T, T> t)
+        {
+            return value => f(t(value));
+        }
+    }
+}
---
+++ Tests/7.3-Compose/ComposeTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+
+namespace IMoreno.FunctionalExercises.Compose.Tests
+{
+    public class ComposeTests
+    {
+        [Fact]
+        public void Compose_two_unary_functions()
+        {
+            int AddOne(int a) => a + 1;
+            int Duplicate(int a) => a * 2;
+
+            var operation = Composition.Of<int>(AddOne, Duplicate);
+
+            operation(4).Should().Be(9);
+        }
+    }
+}

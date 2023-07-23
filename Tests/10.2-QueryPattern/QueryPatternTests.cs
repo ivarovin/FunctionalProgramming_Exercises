95ea9502cb970f4658d69004f8d826bf04a28e59
Iván Moreno <imoreno.owo@gmail.com>
2023-07-23 19:02:16 +0200

    test: 10.2: implement SelectMany for exceptional

--- Tests/10.2-QueryPattern/QueryPatternTests.cs
+++ Tests/10.2-QueryPattern/QueryPatternTests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using IMoreno.FunctionalExercises.ToOption;
+using IMoreno.FunctionalExercises.TryRun;
 
 namespace IMoreno.FunctionalExercises.QueryPattern.Tests;
 
@@ -9,29 +10,45 @@ public class QueryPatternTests
     public void Implement_select_for_either()
     {
         Either<string, int> doc = 1;
-        
+
         var result = from x in doc select x * 2;
-        
+
         result.Match
         (
             _ => Assert.True(false),
             i => i.Should().Be(2)
         );
     }
-    
+
     [Fact]
     public void Implement_SelectMany_for_either()
     {
         Either<string, int> Parse(string x) => int.Parse(x);
-        
-        var result = from x in Parse("1") 
-                     from i in Parse("2")
-                     select x + i;
-        
+
+        var result = from x in Parse("1")
+            from i in Parse("2")
+            select x + i;
+
         result.Match
         (
             _ => Assert.True(false),
             i => i.Should().Be(3)
+        );
+    }
+
+    [Fact]
+    public void Implement_SelectMany_for_exceptional()
+    {
+        Exceptional<int> Parse(string x) => new InvalidOperationException();
+
+        var result = from x in Parse("1")
+            from i in Parse("2")
+            select x + i;
+
+        result.Match
+        (
+            error => error.Should().BeOfType<InvalidOperationException>(),
+            _ => Assert.True(false)
         );
     }
 }
